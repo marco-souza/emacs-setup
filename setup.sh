81bff5669b9cb027939f8e2da04620e42d1c7111
Marco Antônio <marco@paradigm.co>
2021-02-09 11:59:03 -0300

    fea: add cleaning up step to setup script

--- setup.sh
+++ setup.sh
@@ -1,18 +1,25 @@
 #! /bin/bash
+if [[ -e ~/.emacs.d/ ]]; then
+  read -p "Cleanup previous config? - (N/y): " shouldCleanup
+  if [[ $shouldCleanup = "y" ]]; then
+    echo "- Cleaning up..."
+    rm -rf ~/.emacs.d ~/.spacemacs
+  fi
+fi
 if [[ ! -e ~/.emacs.d/ ]]; then
-  echo Installing spacemacs...
-  git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+  echo "- Installing spacemacs..."
+  git clone -q https://github.com/syl20bnr/spacemacs ~/.emacs.d
 fi
 
-echo Copying spacemacs configs...
+echo "- Copying spacemacs configs..."
 spacemacs_file=spacemacs
 user_config_file=settings.org
 src_cmd=""
 if [[ -e ./$spacemacs_file ]] && [[ -e ./$user_config_file ]]; then
-    echo Loading local config
+    echo "- Loading local config..."
     src_cmd='cat ./'
 else
-    echo Loading remote config
+    echo "- Loading remote config..."
     src_cmd='curl https://raw.githubusercontent.com/marco-souza/emacs-setup/main/'
 fi
 
@@ -24,8 +31,8 @@ eval "$src_cmd$user_config_file > $HOME/.emacs.d/$user_config_file"
 init_file=$HOME/.emacs.d/init.el
 if ! grep -Fxq "$user_config_file" $init_file; then
     # code does not found
-    echo Setup init.el settings
+    echo "- Setup init.el settings..."
     echo -e "\n;; Setup user settings.org\n(require 'org)\n(org-babel-load-file (\n\texpand-file-name \"settings.org\" \n\tuser-emacs-directory))" >> $init_file
 fi
 
-echo "Your emacs is ready to go :)"
+echo -e "\nYour spaceemacs is ready to go :)"
